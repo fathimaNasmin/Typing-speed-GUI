8b557130cba1214584b5cda8eb8c7ec5757cbe01
Fathima Nasmin <fathima.nasmin@icloud.com>
2022-10-05 10:57:56 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import customtkinter
+
+window = customtkinter.CTk()
+window.geometry("1200x600")
+window.title("Image Watermarking App")
+
+class MainWindow:
+    def __init__(self):
+        pass
+
+
+
+
+app = MainWindow(window)
+window.mainloop()
